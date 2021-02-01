3f1816017cc4d469c8b453d8e707a6a6f387bda7
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-02-02 02:51:10 +0600

    ADVANCE JAVASCRIPT

---
+++ true-false.js
@@ -0,0 +1,131 @@
+// // False Item List
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// True Item List
+// "0"
+// ' '
+// [ ]
+// {} 
+
+
+
+
+
+
+
+
+const age = 10;
+if(age > 0){
+    console.log('CONDITION IS TRUE');
+}
+else{
+    console.log('CONDITION IS FALSE');
+}
+
+// ANOTHER
+
+const ageS = 0;
+if(ageS > 0){
+    console.log('CONDITION IS TRUE');
+}
+else{
+    console.log('CONDITION IS FALSE');
+}
+
+// ANOTHER
+
+const name = 'SALMANSHA';
+if(name){
+    console.log('CONDITION IS TRUE');
+}else{
+    console.log('CONDITION IS FALSE');
+}
+
+// SOLMATE
+
+const names = 0;
+
+if(names > 0){
+    console.log('This is a True name');
+}else{
+    console.log('This is False name');
+}
+
+// EMPATY STRING
+
+const namess = '';
+
+if(namess > 0){
+    console.log('This a true');
+} else{
+    console.log('This is false');
+}
+
+// SOLMATE(EMPTY STRING)
+
+const namee = ' ';
+
+
+if(namee > 0){
+    console.log('THis is true');
+}else{
+    console.log('THis is false');
+}
+
+
+// undefined
+
+const unDe = undefined;
+console.log(unDe);
+
+if(unDe > 0){
+    console.log('UNDIFINED IS TRUE');
+}else{
+    console.log('UNDEFINED IS FALSE');
+}
+
+// NAL
+
+const naltuVaay = null;
+console.log(naltuVaay);
+
+if(naltuVaay > 0){
+    console.log('NALL VAAY IS TRUE');
+}else{
+    console.log('NALL VAAY IS FALSE');
+}
+
+// NAN
+
+const nanVay = NaN;
+console.log(nanVay);
+if(nanVay > 0){
+    console.log('NAN VAY IS TRUE');
+}else{
+    console.log('NAN VAY IS FALSE');
+}
+
+
+// FALSE
+
+const faltuVay = false;
+console.log(faltuVay);
+if(faltuVay > 0){
+    console.log('faltuVaay is true');
+}else{
+    console.log('faltuVaay is false');
+}
+
+// {},[]
+
+const nanuKaka =[];
+
+if(nanuKaka){
+    console.log('nanukaka is true');
+}else{
+    console.log('nanukaka is false');
+}
