67c8b7b64586020ce2e16672ee4ebc159056307f
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-02-02 15:36:39 +0600

    null VS undefinied

---
+++ null-vs-undifined.js
@@ -0,0 +1,43 @@
+let Sohan;
+console.log(Sohan);
+
+
+
+
+function add(num1, num2){
+    console.log( num1 + num2)
+}
+const result=add(20,30);
+console.log(result);
+
+
+function add(num1, num2){
+    console.log(num2,);
+}
+const result =add(12,);
+console.log(result);
+
+function add(num1, num2 =300){
+    console.log(num1,num2);
+}
+const result = add(200,);
+console.log(result);
+
+
+
+const sohanAddress ={ name:"MD SHOWAN HOSSAIN", phone: "01998-110072", address: "khulna Division"}
+
+console.log(sohanAddress.gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+
+let numbers = [10, 20, 30, 40,];
+console.log(numbers[5]);
+
+
+
+
+
