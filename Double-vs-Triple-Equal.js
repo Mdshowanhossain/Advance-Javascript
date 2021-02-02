b9cc954c74563adcbffc495ce1d1117787a3e16e
Mdshowanhossain <76901439+Mdshowanhossain@users.noreply.github.com>
2021-02-02 15:56:33 +0600

    Update Double-vs-Triple-Equal.js

--- Double-vs-Triple-Equal.js
+++ Double-vs-Triple-Equal.js
@@ -1,62 +1,61 @@
-// const first = 2;
-// const second = 3;
-// if(first==second){
-//     console.log('condition is true');
-// }
-// else{
-//     console.log('condition is false');
-// }
+const first = 2;
+const second = 3;
+if(first==second){
+console.log('condition is true');
+}
+ else{
+    console.log('condition is false');
+}
 
-// const first = 2;
-// const second = 2;
-// if(first==second){
-//     console.log('condition is true');
-// }
-// else{
-//     console.log('condition is false');
-// }
-
-
-// const first = 2;
-// const second = "2";
-// if(first==second){
-//     console.log('condition is true');
-// }
-// else{
-//     console.log('condition is false');
-// }
+const first = 2;
+const second = 2;
+ if(first==second){
+console.log('condition is true');
+}
+else{
+ console.log('condition is false');
+ }
 
 
-
-// const first = 2;
-// const second = "2";
-// if(first===second){
-//     console.log('condition is true');
-// }
-// else{
-//     console.log('condition is false');
-// }
-
-
-// const first = 2;
-// const second = 2;
-// if(first===second){
-//     console.log('condition is true');
-// }
-// else{
-//     console.log('condition is false');
-// }
+const first = 2;
+const second = "2";
+if(first==second){
+    console.log('condition is true');
+}
+else{
+  console.log('condition is false');
+}
 
 
 
-// const first = 1;
-// const second = true;
-// if(first==second){
-//     console.log('condition is true');
-// }
-// else{
-//     console.log('condition is false');
-// }
+const first = 2;
+ const second = "2";
+if(first===second){
+    console.log('condition is true');
+}
+ else{
+    console.log('condition is false');
+}
+
+
+ const first = 2;
+ const second = 2;
+if(first===second){
+    console.log('condition is true');
+ }
+else{
+     console.log('condition is false');
+}
+
+
+
+ const first = 1;
+ const second = true;
+ if(first==second){
+    console.log('condition is true');
+ else{
+   console.log('condition is false');
+}
 
 const first = 0;
 const second = false;
@@ -69,11 +68,11 @@ else{
 
 
 
-// const first = 1;
-// const second = true;
-// if(first===second){
-//     console.log('condition is true');
-// }
-// else{
-//     console.log('condition is false');
-// }
+const first = 1;
+const second = true;
+if(first===second){
+console.log('condition is true');
+ }
+else{
+console.log('condition is false');
+}
